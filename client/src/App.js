15c73cbdf041cde7691b113185a3fb06f51d28ef
karabo-r <karaborabothata0@gmail.com>
2022-02-18 06:48:57 -0800

    Montserrat font added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Landing from "./components/Landing";
 import Qr_Code from "./components/Qr_Code/Qr_Code";
 import Emoji_Search from "./components/Emoji_Search/Emoji_Search";
@@ -7,11 +8,16 @@ const App = () => {
   return (
     <Container>
       <Landing />
-      <Qr_Code />
-      <Emoji_Search />
+      {/* <Qr_Code /> */}
+      {/* <Emoji_Search /> */}
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-family: "Montserrat", sans-serif;
+  height: 100vh;
+  width: 100%;
+  font-weight: 500;
+`;
 export default App;
