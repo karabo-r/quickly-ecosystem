8853f2c937dae8b08f6025e7e802c4d4b0eed7c4
karabo-r <karaborabothata0@gmail.com>
2022-02-18 06:06:21 -0800

    components/ applications folders added + files

---
+++ client/src/components/Emoji_Search/Emoji_Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Emoji_Search = () => {
+  return <div>Emoji_Search</div>;
+};
+
+export default Emoji_Search;
---
+++ client/src/components/Qr_Code/Qr_Code.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Qr_Code = () => {
+  return <div>Qr_Code</div>;
+};
+
+export default Qr_Code;
