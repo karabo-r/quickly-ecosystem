fac82e38ae689553e4e5ef55968f939d04aa2390
karabo-r <karaborabothata0@gmail.com>
2022-02-20 03:44:17 -0800

    overview update - landing page complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import styled from "styled-components";
 import Landing from "./components/Landing";
 import Qr_Code from "./components/Qr_Code/Qr_Code";
 import Emoji_Search from "./components/Emoji_Search/Emoji_Search";
+import Author from "./components/Author";
+import Menu from "./components/Menu";
 
 const App = () => {
   return (
     <Container>
+      <Menu />
       <Landing />
       {/* <Qr_Code /> */}
       {/* <Emoji_Search /> */}
+      <Author />
     </Container>
   );
 };
